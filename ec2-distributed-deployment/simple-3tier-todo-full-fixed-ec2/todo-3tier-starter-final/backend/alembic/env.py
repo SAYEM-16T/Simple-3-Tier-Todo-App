381d01f5ea86f6c10023124451bb370d65978d71
SAYEM_19 <hide5too9@gmail.com>
2026-03-29 11:54:09 +0000

    update pass. syntex

--- ec2-distributed-deployment/simple-3tier-todo-full-fixed-ec2/todo-3tier-starter-final/backend/alembic/env.py
+++ ec2-distributed-deployment/simple-3tier-todo-full-fixed-ec2/todo-3tier-starter-final/backend/alembic/env.py
@@ -8,7 +8,7 @@ from app.db.base import Base
 from app.models import task, user  # noqa: F401
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL.replace("%", "%%"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
